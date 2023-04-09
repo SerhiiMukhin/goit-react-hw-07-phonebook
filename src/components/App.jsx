607ff09516d5fb71dfdd668c6f500bdebea09dfe
SerhiiMukhin <110244388+SerhiiMukhin@users.noreply.github.com>
2023-04-09 13:54:40 +0300

    add spinner

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,6 +6,7 @@ import css from './App.module.css';
 import { useDispatch, useSelector } from 'react-redux';
 import { selectError, selectIsLoading } from 'redux/selectors';
 import { fetchContacts } from 'redux/operations';
+import { RotatingLines } from 'react-loader-spinner';
 
 export const App = () => {
   const dispatch = useDispatch();
@@ -21,7 +22,16 @@ export const App = () => {
       <h1 className={css.main_title}>Phonebook</h1>
       <ContactForm />
       <h2 className={css.contacts_title}>Contacts</h2>
-      {isLoading && !error && <b>Request in progress...</b>}
+
+      {isLoading && !error && (
+        <div className={css.spinner}>
+          <RotatingLines
+            strokeColor="green"
+            animationDuration="0.75"
+            width="25"
+          />
+        </div>
+      )}
 
       {error && <b>Ooops, something went wrong...</b>}
       <Filter />
